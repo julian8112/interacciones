f4532fa394399e0eb1a8c351bb486ea401068de2
julian8112 <134758716+julian8112@users.noreply.github.com>
2023-06-24 07:48:03 -0500

    commit iniciar

---
+++ js/main.js
@@ -0,0 +1,16 @@
+function circulo(){
+    const figura=document.getElementById("figura");
+    figura.classList.toggle("circulo");
+}
+function rectangulo(){
+    const elemento=document.getElementById("figura");
+    elemento.classList.toggle("rectangulo");
+}
+function panel_lateral(){
+    const elemento=document.getElementById("panel-lateral");
+    elemento.classList.toggle("activo");
+}
+function fondo_body(){
+    const elemento=document.getElementById("body");
+    elemento.classList.toggle("img-2");
+}
